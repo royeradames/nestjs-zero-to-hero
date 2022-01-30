7efb68e2a92a53c05b0797e144f202702cd7cf51
Royer Adames <royeraadames@gmail.com>
2022-01-30 14:50:39 -0500

    ✅ config schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 
 /* module needs to be install */
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 /* modules organize the archicture of the nest app
 - everything start in the app.module.ts file (root module)
 - similar to angular, the root module is the entry point of the application
@@ -32,28 +32,74 @@ import { ConfigModule } from '@nestjs/config';
       - the ormconfig.json file is a json file that tells nestjs how to connect to the database
       - this is similar to how postgresql was configured 
       in submodules we use forFeature() and in root module we use forRoot()
+      * example of having the configuration hardcoded
      */
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      /* entites translate to  
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'postgres',
+    //   database: 'task-management',
+    //   /* entites translate to
+    //     - the table in the database
+    //     - schema in the database
+    //     You can load explicity entities or load all entities (autoLoadAllEntities: true)
+    //     - autoLoadAllEntities is cleaner and teacher preferred
+    //   */
+    //   autoLoadEntities: true,
+    //   /* synchronize: true: always keep your database schema in sync
+    //   or you can do manual migrations
+    //     - is an advance case
+    //     -dangerous after you have a database with data
+    //       - solution: migrations with the cli
+    //   */
+    //   synchronize: true,
+    // }),
+    /* 
+    * example of having the configuration in the .env file
+    need to wait for the config module to initialize so we need to wait for the file data */
+    TypeOrmModule.forRootAsync({
+      /* 
+      * 3 most have properties are
+      - imports: []
+      - useFactory: () => ({})
+      - inject: []
+      * import says what module useFactory depends on
+        - It waits for the ConfigModule to be initialized
+       */
+      imports: [ConfigModule],
+
+      /* inject what injections you want in the useFactory like in a service constructor */
+      inject: [ConfigService],
+
+      /* a function call by nestjs when ever we want to initialize this module
+       * you can do dependency injection in the function
+       */
+      useFactory: async (configService: ConfigService) =>
+        /* the return will be the configuration for this module */
+        ({
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          /* entites translate to
         - the table in the database
         - schema in the database
         You can load explicity entities or load all entities (autoLoadAllEntities: true)
         - autoLoadAllEntities is cleaner and teacher preferred
       */
-      autoLoadEntities: true,
-      /* synchronize: true: always keep your database schema in sync 
+          autoLoadEntities: true,
+          /* synchronize: true: always keep your database schema in sync
       or you can do manual migrations
         - is an advance case
         -dangerous after you have a database with data
           - solution: migrations with the cli
       */
-      synchronize: true,
+          synchronize: true,
+        }),
     }),
 
     AuthModule,
